d394c02029aa4f1b2e8030c169afe9f945ee957e
Mayer Florian <florian.mayer@fh-joanneum.at>
2024-01-09 13:07:10 +0100

    New Recorded DAta

--- arduino/processData/model_params.h
+++ arduino/processData/model_params.h
@@ -1,8 +1,8 @@
 #ifndef MODEL_PARAMS_H
 #define MODEL_PARAMS_H
 
-const float input_scale = 0.0857137143611908;
-const int input_zero_point = 10;
+const float input_scale = 0.047196164727211;
+const int input_zero_point = 68;
 const float output_scale = 0.00390625;
 const int output_zero_point = -128;
 
